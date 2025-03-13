b72c6acfc834acb496b1430464954d491cdcf7ce
rm2thaddeus <aitorpatinodiaz@gmail.com>
2025-03-13 20:38:15 +0100

    Initialize visual analysis package

---
+++ src/visual_analysis/__init__.py
@@ -0,0 +1,6 @@
+"""
+Visual Analysis Package for Video Timeline Analyzer.
+
+This package contains modules for visual content analysis,
+including scene context analysis and facial emotion recognition.
+"""
