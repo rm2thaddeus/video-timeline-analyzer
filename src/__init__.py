5aa5b96931dfc2a24a8d5fba3bf2952bce4dde18
rm2thaddeus <aitorpatinodiaz@gmail.com>
2025-03-13 20:17:04 +0100

    Initialize src package

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Video Timeline Analyzer - A sophisticated video analysis application.
+
+This package provides tools for analyzing videos, extracting metadata, and 
+generating interactive timelines with rich scene information.
+"""
+
+__version__ = "0.1.0"
